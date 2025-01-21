eb1646f9f1e552cc8c214c3b89438bbac222469d
Daniil Lutskiy <ya24711@bristol.ac.uk>
2025-01-21 11:39:22 +0000

    a save before enum testing

--- cw-shapes/src/main/java/edu/uob/Shapes.java
+++ cw-shapes/src/main/java/edu/uob/Shapes.java
@@ -4,22 +4,30 @@ public class Shapes {
 
   // TODO use this class as then entry point; play around with your shapes, etc
   public static void main(String[] args) {
-    Triangle testTriangle = new Triangle(5, 7, 9);
+    Triangle EQUILATERALTriangle = new Triangle(8, -1, 4);
+    System.out.println(EQUILATERALTriangle.getVariant());
+
     Triangle myTriangle = new Triangle(1,80,81);
     Triangle myOtherTriangle = new Triangle(8,0,-1);
-    int longestSide1 = testTriangle.getLongestSide();
-    int longestSide2 = myTriangle.getLongestSide();
-    int longestSide3 = myOtherTriangle.getLongestSide();
-    System.out.println("The longest side of the triangle is " + longestSide1);
-    System.out.println("The longest side of the triangle is " + longestSide2);
-    System.out.println("The longest side of the triangle is " + longestSide3);
-    System.out.println(myOtherTriangle.toString());
+//    int longestSide1 = testTriangle.getLongestSide();
+//    int longestSide2 = myTriangle.getLongestSide();
+//    int longestSide3 = myOtherTriangle.getLongestSide();
+//    System.out.println("The longest side of the triangle is " + longestSide1);
+//    System.out.println("The longest side of the triangle is " + longestSide2);
+//    System.out.println("The longest side of the triangle is " + longestSide3);
+//    System.out.println(myOtherTriangle.toString());
+//
+//    TwoDimensionalShape thing = new Triangle(4, 5, 6 );
+//    System.out.println(thing.toString());
+//
+//    myTriangle.setColour(Colour.RED);
+//    System.out.println(myTriangle.getColour());
 
-    TwoDimensionalShape thing = new Triangle(4, 5, 6 );
-    System.out.println(thing.toString());
 
-    myTriangle.setColour(Colour.RED);
-    System.out.println(myTriangle.getColour());
+
+
+
+
   }
 
 
--- cw-shapes/src/main/java/edu/uob/Triangle.java
+++ cw-shapes/src/main/java/edu/uob/Triangle.java
@@ -2,12 +2,36 @@ package edu.uob;
 
 public class Triangle extends TwoDimensionalShape {
   private int a, b, c;
+  private TriangleVariant triangleShape;
 
   // TODO implement me!
   public Triangle(int e, int f, int g) {
     this.a = e;
     this.b = f;
     this.c = g;
+
+
+    if (a == b || b == c || c == a) {
+      triangleShape = TriangleVariant.ISOSCELES;
+    } if (a == b && b == c) {
+      triangleShape = TriangleVariant.EQUILATERAL;
+    } if (a != b && b != c && c != a) {
+      triangleShape = TriangleVariant.SCALENE;
+    } if ((Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2)) ||
+          (Math.pow(c, 2) + Math.pow(b, 2) == Math.pow(a, 2)) ||
+          (Math.pow(a, 2) + Math.pow(c, 2) == Math.pow(b, 2))){
+      triangleShape = TriangleVariant.RIGHT;
+    } if ((a + b == c) || (b + c == a) || (c + a == b)){
+      triangleShape = TriangleVariant.FLAT;
+    } if ((a + b < c) || (b + c < a) || (c + a < b)){
+      triangleShape = TriangleVariant.IMPOSSIBLE;
+    } if ((a < 0) || (b < 0) || (c < 0)){
+      triangleShape = TriangleVariant.ILLEGAL;
+    }
+  }
+
+  public TriangleVariant getVariant(){
+    return triangleShape;
   }
 
   public int getLongestSide() {
