3d2b4dcba130eb4822ed3d4f4eac8519795b116d
Daniil Lutskiy <ya24711@bristol.ac.uk>
2025-01-21 12:16:20 +0000

    migrating stuff from the Java directory

---
+++ IntelliJ Template/cw-shapes/src/main/java/edu/uob/Shapes.java
@@ -0,0 +1,11 @@
+package edu.uob;
+
+public class Shapes {
+
+  // TODO use this class as then entry point; play around with your shapes, etc
+  public static void main(String[] args) {
+    Rectangle r = new Rectangle(1,5);
+
+    System.out.println("Hello world! my rectangle is " + r.height + " high");
+  }
+}
